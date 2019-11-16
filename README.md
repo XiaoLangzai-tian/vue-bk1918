5ebd3c8ad5550445bf81dab151b1009d7fbcca87
BK1918TianHeming <mr_xiaolangzai@163.com>
2019-11-16 11:01:24 +0800

    项目初始化

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# project
+#技术栈
+>   -axios
+    - vue-router
+    - vuex
+    - JS组件
+    - UI组件
+    - vue-touch
+    - vue-lazyLoad
+    - better-scroll
 
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+#环境配置
+>vue-cli@3
+vue.config.js
---
+++ vue.config.js
@@ -0,0 +1,30 @@
+    const path =require('path');
+
+    module.exports={
+        // 跨域
+        devServer:{
+            proxy:{
+                '/api':{
+                    taeget:"http://39.97.33.178",
+                    changeOrigin:true
+                }
+            }
+        },
+        //别名配置
+        configureWebpack:{
+            resolve:{
+                alias:{
+                    //@代表./src
+                    "@":path.join(__dirname,"./src"),
+                    "@api":path.join(__dirname,"./src/api"),
+                    "@common":path.join(__dirname,"./src/common"),
+                    "@components":path.join(__dirname,"./src/components"),
+                    "@pages":path.join(__dirname,"./src/pages"),
+                    "@router":path.join(__dirname,"./src/touter"),
+                    "@store":path.join(__dirname,"./src/store"),
+                    "@utils":path.join(__dirname,"./src/utils"),
+                    
+                }
+            }
+        }
+    }
